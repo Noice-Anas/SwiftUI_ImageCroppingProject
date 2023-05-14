a5c4bd829e1176b0c82e9289dcba38dc63a391c7
Noice_Anas <anas.alhalabi2002@gmail.com>
2023-05-14 15:45:33 +0300

    Initial Commit

---
+++ SwiftUI_ImageCroppingProject/SwiftUI_ImageCroppingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ImageCroppingProjectApp.swift
+//  SwiftUI_ImageCroppingProject
+//
+//  Created by Noice_anas on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ImageCroppingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
